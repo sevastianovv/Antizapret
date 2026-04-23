741d2d1aebf7eac567ad5fe9a7000a72ff445034
sevastianovv <50197320+sevastianovv@users.noreply.github.com>
2026-04-23 18:43:21 +0300

    Add files via upload

---
+++ install_panel.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+set -euo pipefail
+exec bash <(wget -qO- https://raw.githubusercontent.com/sevastianovv/Antizapret/main/install_adminantizapret_noprompt.sh)
---
+++ install_vpn.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+set -euo pipefail
+exec bash <(wget -qO- https://raw.githubusercontent.com/sevastianovv/Antizapret/main/antizapret_setup_autorun.sh)
